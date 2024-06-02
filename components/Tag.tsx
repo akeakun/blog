bf3aae840b49de819402773d8077d2a2be33f338
akeakun <140699318+akeakun@users.noreply.github.com>
2024-06-03 00:58:53 +0600

    Fix exlint issue in Shadcn Badge

--- components/Tag.tsx
+++ components/Tag.tsx
@@ -9,7 +9,7 @@ const Tag = ({ text }: Props) => {
   return (
     <Link
       href={`/tags/${slug(text)}`}
-      className="mr-3 text-sm font-medium uppercase text-gray-800 py-2"
+      className="mr-3 py-2 text-sm font-medium uppercase text-gray-800"
     >
       <Badge className="bg-primary-500 hover:bg-primary-600 dark:hover:bg-primary-400">
         {text.split(' ').join('-')}
